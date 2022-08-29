7ac38249ada6e2474f10208d34f49002cd9ce1cd
Ameya Rao <ameyarao98@gmail.com>
2022-08-29 13:49:08 +0100

    temp

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to the mining game!</h1>
+
+<style>
+	:global(body) {
+		background-image: url('mining.gif');
+		background-size: cover;
+		height: 100vh;
+		padding: 0;
+		margin: 0;
+	}
+	h1 {
+		margin-top: 0;
+		-webkit-text-stroke: 0.3px black;
+		font-size: 10vh;
+		text-align: center;
+		font-family: 'Silkscreen', sans-serif;
+		font-weight: bold;
+		color: #ffd90f;
+	}
+</style>
