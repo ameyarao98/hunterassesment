e67e8ae53a0d4cd3e4d31af9d6cfee6163124c44
Ameya Rao <ameyarao98@gmail.com>
2022-08-30 15:34:31 +0100

    cleanup

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
-	import { GraphQLClient, gql } from 'graphql-request';
 
 	let gameJoined: boolean;
 
@@ -12,19 +11,7 @@
 	let loginUsername: string;
 	let loginPassword: string;
 
-	let graphqlClient: GraphQLClient;
-	let userResourceData: {
-		resourceName: string;
-		factoryLevel: number;
-		amount: number;
-		productionRate: number;
-		timeUntilUpgradeComplete: number;
-	}[];
-	let factoryData: {
-		iron: { factoryLevel: number; productionPerSecond: number; nextUpgradeDuration: number }[];
-		copper: { factoryLevel: number; productionPerSecond: number; nextUpgradeDuration: number }[];
-		gold: { factoryLevel: number; productionPerSecond: number; nextUpgradeDuration: number }[];
-	};
+	let resourceData: { resourceName: string; probability: number; color: string }[];
 
 	async function signup(username: string, password: string) {
 		const response = await fetch('http://localhost:8000/signup', {
@@ -80,83 +67,7 @@
 				}
 			});
 			jwt = await response.text();
-			await setupGame();
 		}
-	}
-
-	async function setupGame() {
-		graphqlClient = new GraphQLClient('http://localhost:3000/graphql', {
-			headers: { creds: jwt }
-		});
-		await enterGame();
-		await getFactoryData();
-		gameJoined = true;
-	}
-
-	async function enterGame() {
-		const query = gql`
-			mutation {
-				enterGame
-			}
-		`;
-		try {
-			await graphqlClient.request(query);
-			toast.push("New factories created!", {
-				theme: {
-					'--toastBackground': '#077023',
-					'--toastColor': 'white',
-					'--toastBarBackground': 'white'
-				}
-			});
-		} catch (error) {
-			toast.push("Factories restored!", {
-				theme: {
-					'--toastBackground': '#077023',
-					'--toastColor': 'white',
-					'--toastBarBackground': 'white'
-				}
-			});
-		}
-	}
-
-	async function getUserResourceData() {
-		const query = gql`
-			query {
-				userResourceData {
-					resourceName
-					factoryLevel
-					amount
-					productionRate
-					timeUntilUpgradeComplete
-				}
-			}
-		`;
-		userResourceData = (await graphqlClient.request(query)).userResourceData;
-	}
-
-	async function getFactoryData() {
-		const query = gql`
-			query {
-				factoryData {
-					iron {
-						factoryLevel
-						productionPerSecond
-						nextUpgradeDuration
-					}
-					copper {
-						factoryLevel
-						productionPerSecond
-						nextUpgradeDuration
-					}
-					gold {
-						factoryLevel
-						productionPerSecond
-						nextUpgradeDuration
-					}
-				}
-			}
-		`;
-		factoryData = (await graphqlClient.request(query)).factoryData;
 	}
 </script>
 
@@ -177,61 +88,20 @@
 {/if}
 
 {#if gameJoined}
-	<div class="factoryData">
-		<table class="ironFactoryData">
-			<tr>
-				<th colspan="3">Iron</th>
+	<table class="resourceData">
+		<tr>
+			<th>Resource</th>
+			<th>Probability Rate</th>
+		</tr>
+		{#each resourceData as { resourceName, probability, color }}
+			<tr style="background-color: #{color}">
+				<th>{resourceName}</th>
+				<th>{probability}/s</th>
 			</tr>
-			<tr>
-				<th>Level</th>
-				<th>Production Rate</th>
-				<th>Next Upgrade Duration</th>
-			</tr>
-			{#each factoryData.iron as { factoryLevel, productionPerSecond, nextUpgradeDuration }}
-				<tr>
-					<th>{factoryLevel}</th>
-					<th>{productionPerSecond}/s</th>
-					<th>{nextUpgradeDuration} seconds</th>
-				</tr>
-			{/each}
-		</table>
-		<table class="copperFactoryData">
-			<tr>
-				<th colspan="3">Copper</th>
-			</tr>
-			<tr>
-				<th>Level</th>
-				<th>Production Rate</th>
-				<th>Next Upgrade Duration</th>
-			</tr>
-			{#each factoryData.copper as { factoryLevel, productionPerSecond, nextUpgradeDuration }}
-				<tr>
-					<th>{factoryLevel}</th>
-					<th>{productionPerSecond}/s</th>
-					<th>{nextUpgradeDuration} seconds</th>
-				</tr>
-			{/each}
-		</table>
-		<table class="goldFactoryData">
-			<tr>
-				<th colspan="3">Gold</th>
-			</tr>
-			<tr>
-				<th>Level</th>
-				<th>Production Rate</th>
-				<th>Next Upgrade Duration</th>
-			</tr>
-			{#each factoryData.gold as { factoryLevel, productionPerSecond, nextUpgradeDuration }}
-				<tr>
-					<th>{factoryLevel}</th>
-					<th>{productionPerSecond}/s</th>
-					<th>{nextUpgradeDuration} seconds</th>
-				</tr>
-			{/each}
-		</table>
-	</div>
-	<p class="jwt">{jwt}</p>
+		{/each}
+	</table>
 {/if}
+<p class="jwt">{jwt}</p>
 
 <SvelteToast />
 
@@ -267,13 +137,6 @@
 		right: 1%;
 	}
 
-	.factoryData {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		bottom: 2%;
-		position: absolute;
-	}
 	table {
 		display: inline-table;
 		margin-left: 5vw;
@@ -282,17 +145,6 @@
 	th,
 	tr {
 		border: 1px solid black;
-	}
-
-	.ironFactoryData {
-		background-color: #a19d94;
-	}
-	.copperFactoryData {
-		background-color: #b87333;
-	}
-
-	.goldFactoryData {
-		background-color: #ffd700;
 	}
 
 	.jwt {
